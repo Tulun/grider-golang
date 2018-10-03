a6b085d52fd9c7efb9446e1c3720cefeb2df023d
Jason Kiraly <jaykiraly@gmail.com>
2018-10-03 13:57:55 -0700

    created newCard func, returns string.

--- section-3/main.go
+++ section-3/main.go
@@ -7,7 +7,11 @@ import (
 func main() {
 	// These are equivalent declarations
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
