4b2ac50ddf24518460bbb646e630c3bc1f958c95
Jason Kiraly <jaykiraly@gmail.com>
2018-10-03 13:48:41 -0700

    started section 3.

---
+++ section-3/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// These are equivalent declarations
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+
+	fmt.Println(card)
+}
