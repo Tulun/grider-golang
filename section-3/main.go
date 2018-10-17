dddf0fd9084d51e8448278eb31636d7a69f4bd17
Jason Kiraly <jaykiraly@gmail.com>
2018-10-17 11:35:12 -0700

    finished lesson 26.

--- section-3/main.go
+++ section-3/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	// cards := deck{"Ace of Diamonds", newCard()}
-	// cards = append(cards, "Six of Spades")
-
 	cards := newDeck()
 
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
