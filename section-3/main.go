082024d35696b1a9c1137d369697e6328145739a
Jason Kiraly <jaykiraly@gmail.com>
2018-10-17 11:11:13 -0700

    convert deck to byte slice.

--- section-3/main.go
+++ section-3/main.go
@@ -1,17 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// cards := deck{"Ace of Diamonds", newCard()}
 	// cards = append(cards, "Six of Spades")
 
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
-}
-
-func deal(d deck, handSize int) (deck, deck) {
-	return d[:handSize], d[handSize:]
+	// hand.print()
+	// remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
