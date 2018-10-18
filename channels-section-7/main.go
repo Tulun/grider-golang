2f481a4b532f943470e55c3701dcc930f000fc01
Jason Kiraly <jaykiraly@gmail.com>
2018-10-18 14:43:26 -0700

    works

--- channels-section-7/main.go
+++ channels-section-7/main.go
@@ -1,16 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	links := []string {
-		"http://google.com"
-		"http://facebook.com"
-		"http://stackoverflow.com"
-		"http://golang.org"
-		"https://amazon.com"
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"https://amazon.com",
 	}
 
 	for _, link := range links {
-
+		checkLink(link)
 	}
 }
 
+func checkLink(l string) {
+	_, err := http.Get(l)
+	if err != nil {
+		fmt.Println(l, "might be down!")
+		return
+	}
+
+	fmt.Println(l, "is up!")
+}
