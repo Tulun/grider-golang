1489664aaf8b07f65fa29f03e6fc7a9f27924da1
Jason Kiraly <jaykiraly@gmail.com>
2018-10-18 11:42:44 -0700

    Custom Writer implementation.?

--- interfaces-section-6/http/main.go
+++ interfaces-section-6/http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("https://google.com")
 
@@ -20,5 +22,15 @@ func main() {
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	// 2nd implementation
+	// io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes", len(bs))
+	return len(bs), nil
 }
